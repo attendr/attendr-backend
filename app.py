ff65942c946b41766a3b360b9a14f7b5e1fa214c
Anshuman Agarwal <dev.anshuman73@gmail.com>
2019-04-15 16:37:20 +0530

    login endpoint finish

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_restful import Api
+import os
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_CHANGES'] = False
 
 db = SQLAlchemy(app)
--- endpoints.py
+++ endpoints.py
@@ -18,46 +18,21 @@ class Login(Resource):
         db.session.add(login_log)
         db.session.commit()
         if login_log.token_issued:
-
+            courses = []
+            for course in student.courses_taken:
+                attendance_list = [course_class.attendance.attended for course_class in course.classes]
+                attendance_percentage = 100 * attendance_list.count(True) / len(attendance_list)
+                attendance_percentage = int("{0:.2f}".format(attendance_percentage))
+                courses.append({'course_name': course.course_name, 'course_attendance': attendance_percentage})
             result = {
                 'sucess': True,
                 'auth_token': login_log.token_issued,
                 'user': {
                     'username': username,
                     'no_of_courses': len(student.courses_taken),
-                    'average_attendance': 
-                }
-                'courses': []
-            }
-            return result, 200
-        else:
-            result = {
-                'success': False,
-                'message': 'username/password mismatch'
-            }
-            return result, 401
-
-class Classes(Resource):
-    def __init__(self):
-        self.parser = reqparse.RequestParser()
-        self.parser.add_argument('username', type=str, required=True, help='Parmeter username is required')
-        self.parser.add_argument('password', type=str, required=True, help='Parameter password is required')
-        super()
-
-    def post(self):
-        args = self.parser.parse_args(strict=True)
-        username, password = args.get('username'), args.get('password')
-        student = Student.query.filter_by(username=username).first()
-        login_log = LoginLogs(student.id)
-        db.session.add(login_log)
-        db.session.commit()
-        if login_log.token_issued:
-            result = {
-                'sucess': True,
-                'auth_token': login_log.token_issued,
-                'user': {
-                    'username': username
-                }
+                    'average_attendance': int("{0:.2f}".format(sum([course.attendance_percentage for course in courses]) / len(courses)))
+                },
+                'courses': courses
             }
             return result, 200
         else:
